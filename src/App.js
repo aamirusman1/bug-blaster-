e9d14704c8df9fb08d4ad2e124398793894976e9
aamir <aamir6582@gmail.com>
2024-11-03 10:07:51 +0530

    Sixth Stage - implement sorting

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import { useReducer } from "react";
 import TicketForm from "./components/TicketForm.js";
 import ticketReducer from "./reducers/ticketReducer.js";
 import TicketList from "./components/TicketList.js";
+import { sortTickets } from "./utilities/sortingUtilities";
 
 function App() {
   const initialState = {
     tickets: [],
     editingTicket: null, //To know which ticket we are editing
+    sortPreference: "High to Low",
   };
 
   const [state, dispatch] = useReducer(ticketReducer, initialState);
+
+  const sortedTickets = sortTickets(state.tickets, state.sortPreference);
 
   return (
     <div className="App">
@@ -25,8 +29,19 @@ function App() {
           state.tickets.length > 0 && (
             <div className="results">
               <h2>All Tickets</h2>
+
+              <select
+                value={state.sortPreference}
+                onChange={(e) =>
+                  dispatch({ type: "SET_SORTING", payload: e.target.value })
+                }
+              >
+                <option value="High to Low">High to Low</option>
+                <option value="Low to High">Low to High</option>
+              </select>
+
               <TicketList
-                tickets={state.tickets}
+                tickets={sortedTickets}
                 dispatch={dispatch}
               ></TicketList>
             </div>
